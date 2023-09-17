4a89ca8ed2f179dc8f9d1e8b7aef18f807541cc7
mark <mdnpags@gmail.com>
2023-09-17 22:40:20 +0800

    Final

--- index.js
+++ index.js
@@ -4,33 +4,42 @@ for (var i = 0; i < numberOfGunlButtons; i++) {
     document.querySelectorAll(".gun")[i].addEventListener("click", function() {
 
         let buttons = this.innerText;
+        buttonAnimation(buttons)
+        playSound(buttons);
+    })
+}
+document.addEventListener("keydown",function(e){
+    buttonAnimation(e.key)
+    playSound(e.key);
+})
 
-        switch (buttons) {
-            case "Ak47":
+        function playSound(key){
+        switch (key) {
+            case "a":
                 var Ak47 = new Audio ("Sound/AK47.mp3");
                 Ak47.play()
                 break;
         
-            case "Colt":
+            case "t":
                 var Colt = new Audio ("Sound/colt.mp3"); Colt.play();
                 break;
 
-            case "BarrettM8":
+            case "b":
                 var BarrettM8 = new Audio ("Sound/BarretM8.mp3");
                 BarrettM8.play();
                 break;
 
-            case "Machine":
+            case "m":
                 var Machine = new Audio ("Sound/MachineGun.mp3");
                 Machine.play();
                 break;
 
-                case "Bazooka":
+                case "z":
                 var Bazooka = new Audio ("Sound/Bazooka.mp3");
                 Bazooka.play();
                 break;
 
-                case "Carbine":
+                case "c":
                 var Carbine = new Audio ("Sound/Carbine.mp3");
                 Carbine.play();
                 break;
@@ -38,6 +47,11 @@ for (var i = 0; i < numberOfGunlButtons; i++) {
             default:
                 break;
         }
-
-    });
+}
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+    activeButton.classList.remove("pressed");
+    }, 100);
 }
